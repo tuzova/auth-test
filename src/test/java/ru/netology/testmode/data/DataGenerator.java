3cdf96a6d9f2a31b55cf6f286fdf707ca25d382a
Irine Tuzova <alexandrinius@outlook.com>
2021-10-29 19:27:42 +0300

    Initial commit

---
+++ src/test/java/ru/netology/testmode/data/DataGenerator.java
@@ -0,0 +1,87 @@
+package ru.netology.testmode.data;
+
+import com.github.javafaker.Faker;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import lombok.Value;
+
+import java.util.Locale;
+
+import static io.restassured.RestAssured.given;
+
+// cпецификация запроса
+public class DataGenerator {
+    private static final RequestSpecification requestSpec = new RequestSpecBuilder()
+            .setBaseUri("http://localhost")
+            .setPort(7777)
+            .setAccept(ContentType.JSON)
+            .setContentType(ContentType.JSON)
+            .log(LogDetail.ALL)
+            .build();
+
+    private static final Faker faker = new Faker(new Locale("ru"));
+
+
+    private DataGenerator() {
+    }
+
+    // шаблон запроса
+    private static void sendRequest(User user) {
+        given()
+                .spec(requestSpec) // cпецификация запроса
+                .body(new User("vasya", "password", "active")) // передаём объект User
+                .when()
+                .post("/api/system/users")
+                .then()
+                .statusCode(200);
+    }
+
+    // шаблон объекта User
+    @Value
+    public static class User {
+        String login;
+        String password;
+        String status;
+    }
+
+    // рандомный логин юзера
+    public static String getRandomLogin() {
+        String login = faker.name().username();
+        return login;
+    }
+
+    // рандомный пароль юзера
+    public static String getRandomPassword() {
+        String password = faker.internet().password();
+        return password;
+    }
+
+    public static class Registration {
+        private Registration() {
+        }
+
+        // создаем активного юзера на основе шаблона
+        public static User getActiveUser(String status) {
+            User activeUser = new User("vasya", "password", "active");
+            sendRequest(activeUser);
+            return activeUser;
+        }
+
+        // создаем заблокированного юзера на основе шаблона
+        public static User getBlockedUser(String status) {
+            User blockedUser = new User("vasya", "password", "blocked");
+            sendRequest(blockedUser);
+            return blockedUser;
+        }
+
+        // создаем незарегистрированного юзера на основе шаблона
+        public static User getNotRegisteredUser(String status) {
+            User notRegisteredUser = new User(getRandomLogin(), getRandomPassword(), "active");
+            sendRequest(notRegisteredUser);
+            return notRegisteredUser;
+        }
+    }
+}
+
---
+++ src/test/java/ru/netology/testmode/test/AuthTest.java
@@ -0,0 +1,88 @@
+package ru.netology.testmode.test;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+import static ru.netology.testmode.data.DataGenerator.Registration.getActiveUser;
+import static ru.netology.testmode.data.DataGenerator.Registration.getBlockedUser;
+import static ru.netology.testmode.data.DataGenerator.Registration.getNotRegisteredUser;
+import static ru.netology.testmode.data.DataGenerator.getRandomLogin;
+import static ru.netology.testmode.data.DataGenerator.getRandomPassword;
+
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Condition.visible;
+
+class AuthTest {
+
+    @BeforeEach
+    void setup() {
+        Configuration.browser = "chrome";
+        open("http://localhost:7777");
+    }
+
+    @Test
+    @DisplayName("Should successfully login with active registered user")
+    void shouldSuccessfulLoginIfRegisteredActiveUser() {
+        var activeUser = getActiveUser("active");
+        $("[data-test-id='login'] input").setValue(activeUser.getLogin());
+        $("[data-test-id='password'] input").setValue(activeUser.getPassword());
+        $$("button").find(exactText("Продолжить")).click();
+        open("http://localhost:7777/dashboard");
+    }
+
+    @Test
+    @DisplayName("Should get error message if login with not registered user")
+    void shouldGetErrorIfNotRegisteredUser() {
+        var notRegisteredUser = getNotRegisteredUser("active");
+        $("[data-test-id='login'] input").setValue(notRegisteredUser.getLogin());
+        $("[data-test-id='password'] input").setValue(notRegisteredUser.getPassword());
+        $$("button").find(exactText("Продолжить")).click();
+        $(withText("Ошибка")).shouldBe(visible, Duration.ofSeconds(15));
+        $("[data-test-id='error-notification'] div.notification__content").shouldBe(visible);
+        $(withText("Неверно указан логин или пароль")).shouldBe(visible);
+    }
+
+    @Test
+    @DisplayName("Should get error message if login with blocked registered user")
+    void shouldGetErrorIfBlockedUser() {
+        var blockedUser = getBlockedUser("blocked");
+        $("[data-test-id='login'] input").setValue(blockedUser.getLogin());
+        $("[data-test-id='password'] input").setValue(blockedUser.getPassword());
+        $$("button").find(exactText("Продолжить")).click();
+        open("http://localhost:7777/dashboard"); // нет ошибки заблокированного пользователя!!! > переход в лк
+    }
+
+    @Test
+    @DisplayName("Should get error message if login with wrong login")
+    void shouldGetErrorIfWrongLogin() {
+        var activeUser = getActiveUser("active");
+        var wrongLogin = getRandomLogin();
+        $("[data-test-id='login'] input").setValue(wrongLogin);
+        $("[data-test-id='password'] input").setValue(activeUser.getPassword());
+        $$("button").find(exactText("Продолжить")).click();
+        $(withText("Ошибка")).shouldBe(visible, Duration.ofSeconds(15));
+        $("[data-test-id='error-notification'] div.notification__content").shouldBe(visible);
+        $(withText("Неверно указан логин или пароль")).shouldBe(visible);
+    }
+
+    @Test
+    @DisplayName("Should get error message if login with wrong password")
+    void shouldGetErrorIfWrongPassword() {
+        var activeUser = getActiveUser("active");
+        var wrongPassword = getRandomPassword();
+        $("[data-test-id='login'] input").setValue(activeUser.getLogin());
+        $("[data-test-id='password'] input").setValue(wrongPassword);
+        $$("button").find(exactText("Продолжить")).click();
+        $(withText("Ошибка")).shouldBe(visible, Duration.ofSeconds(15));
+        $("[data-test-id='error-notification'] div.notification__content").shouldBe(visible);
+        $(withText("Неверно указан логин или пароль")).shouldBe(visible);
+    }
+}
+
